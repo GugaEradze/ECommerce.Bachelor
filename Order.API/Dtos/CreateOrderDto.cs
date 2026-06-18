ac0e0cf81b46a530d063d37779586a871c18e930
GugaEradze <gugaeradze1833@gmail.com>
2026-06-18 13:17:15 +0400

    implement saga state models and request DTOs

---
+++ Order.API/Dtos/CreateOrderDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Order.API.Dtos
+{
+    public record CreateOrderDto(string ProductId, int Quantity, decimal Price);
+}
---
+++ Order.API/StateMachines/OrderSagaState.cs
@@ -0,0 +1,15 @@
+﻿using MassTransit;
+
+namespace Order.API.StateMachines
+{
+    public class OrderSagaState : SagaStateMachineInstance
+    {
+        public Guid CorrelationId { get; set; }
+        public string CurrentState { get; set; } = string.Empty;
+
+
+        public string ProductId { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
