fd471249461c0c3f19139e39a3a555d16f78f248
GugaEradze <gugaeradze1833@gmail.com>
2026-06-18 14:03:05 +0400

    MongoDB schema and base consumer abstraction

---
+++ Notification.API/Consumers/BaseNotificationConsumer.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using Notification.API.Models;
+
+namespace Notification.API.Consumers
+{
+    public abstract class BaseNotificationConsumer
+    {
+        protected readonly IMongoCollection<NotificationLog> _collection;
+
+        protected BaseNotificationConsumer(IMongoDatabase database)
+        {
+            _collection = database.GetCollection<NotificationLog>("Logs");
+        }
+
+        protected async Task SaveLog(Guid orderId, string message)
+        {
+            var log = new NotificationLog
+            {
+                OrderId = orderId,
+                Message = message,
+                Timestamp = DateTime.UtcNow
+            };
+            await _collection.InsertOneAsync(log);
+        }
+    }
+}
---
+++ Notification.API/Models/NotificationLog.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Notification.API.Models
+{
+    public class NotificationLog
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("orderId")]
+        [BsonRepresentation(BsonType.String)]
+        public Guid OrderId { get; set; }
+
+        [BsonElement("message")]
+        public string Message { get; set; } = string.Empty;
+
+        [BsonElement("timestamp")]
+        public DateTime Timestamp { get; set; }
+    }
+}
